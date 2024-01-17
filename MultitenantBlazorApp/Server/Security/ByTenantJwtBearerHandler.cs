d57ba93b4a2fdea22457fb264250f0b22014f9d8
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-01-17 11:36:17 +0100

    Add comment (TODO) on multitenant part to follow for token validation

--- MultitenantBlazorApp/Server/Security/ByTenantJwtBearerHandler.cs
+++ MultitenantBlazorApp/Server/Security/ByTenantJwtBearerHandler.cs
@@ -20,7 +20,9 @@ namespace MultitenantBlazorApp.Server;
 /// </summary>
 public class ByTenantJwtBearerHandler : JwtBearerHandler
 {
+  // TODO - <Specific Multitenant>
   private readonly IJwtBearerOptionsProvider _jwtBearerOptionsProvider;
+  // TODO - </Specific Multitenant>
 
   /// <summary>
   /// Constructor
@@ -36,16 +38,17 @@ public class ByTenantJwtBearerHandler : JwtBearerHandler
       ILoggerFactory logger,
       UrlEncoder encoder,
       ISystemClock clock,
+      // TODO - <Specific Multitenant>
       IJwtBearerOptionsProvider jwtBearerOptionsProvider)
+      // TODO - </Specific Multitenant>
       : base(options, logger, encoder, clock)
   {
+    // TODO - <Specific Multitenant>
     Guard.IsNotNull(jwtBearerOptionsProvider);
 
     _jwtBearerOptionsProvider = jwtBearerOptionsProvider;
+    // TODO - </Specific Multitenant>
   }
-
-  /// <inheritdoc />
-  protected override Task<object> CreateEventsAsync() => Task.FromResult<object>(new JwtBearerEvents());
 
   /// <summary>
   /// Searches the 'Authorization' header for a 'Bearer' token. If the 'Bearer' token is found, it is validated using <see cref="TokenValidationParameters"/> set in the options.
@@ -56,8 +59,10 @@ public class ByTenantJwtBearerHandler : JwtBearerHandler
     string? token;
     try
     {
+      // TODO - <Specific Multitenant>
       // Add specific options from tenant
       _jwtBearerOptionsProvider.ConfigureOptions(Options);
+      // TODO - </Specific Multitenant>
 
       // Give application opportunity to find from a different location, adjust, or reject token
       var messageReceivedContext = new MessageReceivedContext(Context, Scheme, Options);
@@ -94,6 +99,7 @@ public class ByTenantJwtBearerHandler : JwtBearerHandler
         }
       }
 
+      // TODO - <Specific Multitenant>
       if (Options.Authority == null)
       {
         return AuthenticateResult.NoResult();
@@ -109,7 +115,7 @@ public class ByTenantJwtBearerHandler : JwtBearerHandler
         validationParameters.IssuerSigningKeys = validationParameters.IssuerSigningKeys?.Concat(currentConfiguration.SigningKeys)
             ?? currentConfiguration.SigningKeys;
       }
-
+      // TODO - </Specific Multitenant>
 
       List<Exception>? validationFailures = null;
       SecurityToken? validatedToken = null;
@@ -187,7 +193,9 @@ public class ByTenantJwtBearerHandler : JwtBearerHandler
         return AuthenticateResult.Fail(authenticationFailedContext.Exception);
       }
 
+      // TODO - <Specific Multitenant>
       return AuthenticateResult.Fail("No SecurityTokenValidator available for token: " + token ?? "[null]");
+      // TODO - </Specific Multitenant>
     }
     catch (Exception ex)
     {
@@ -217,139 +225,5 @@ public class ByTenantJwtBearerHandler : JwtBearerHandler
       return null;
     }
     return dateTime;
-  }
-
-  /// <inheritdoc />
-  protected override async Task HandleChallengeAsync(AuthenticationProperties properties)
-  {
-    var authResult = await HandleAuthenticateOnceSafeAsync();
-    var eventContext = new JwtBearerChallengeContext(Context, Scheme, Options, properties)
-    {
-      AuthenticateFailure = authResult?.Failure
-    };
-
-    // Avoid returning error=invalid_token if the error is not caused by an authentication failure (e.g missing token).
-    if (Options.IncludeErrorDetails && eventContext.AuthenticateFailure != null)
-    {
-      eventContext.Error = "invalid_token";
-      eventContext.ErrorDescription = CreateErrorDescription(eventContext.AuthenticateFailure);
-    }
-
-    await Events.Challenge(eventContext);
-    if (eventContext.Handled)
-    {
-      return;
-    }
-
-    Response.StatusCode = 401;
-
-    if (string.IsNullOrEmpty(eventContext.Error) &&
-        string.IsNullOrEmpty(eventContext.ErrorDescription) &&
-        string.IsNullOrEmpty(eventContext.ErrorUri))
-    {
-      Response.Headers.Append(HeaderNames.WWWAuthenticate, Options.Challenge);
-    }
-    else
-    {
-      // https://tools.ietf.org/html/rfc6750#section-3.1
-      // WWW-Authenticate: Bearer realm="example", error="invalid_token", error_description="The access token expired"
-      var builder = new StringBuilder(Options.Challenge);
-      if (Options.Challenge.IndexOf(' ') > 0)
-      {
-        // Only add a comma after the first param, if any
-        builder.Append(',');
-      }
-      if (!string.IsNullOrEmpty(eventContext.Error))
-      {
-        builder.Append(" error=\"");
-        builder.Append(eventContext.Error);
-        builder.Append('\"');
-      }
-      if (!string.IsNullOrEmpty(eventContext.ErrorDescription))
-      {
-        if (!string.IsNullOrEmpty(eventContext.Error))
-        {
-          builder.Append(',');
-        }
-
-        builder.Append(" error_description=\"");
-        builder.Append(eventContext.ErrorDescription);
-        builder.Append('\"');
-      }
-      if (!string.IsNullOrEmpty(eventContext.ErrorUri))
-      {
-        if (!string.IsNullOrEmpty(eventContext.Error) ||
-            !string.IsNullOrEmpty(eventContext.ErrorDescription))
-        {
-          builder.Append(',');
-        }
-
-        builder.Append(" error_uri=\"");
-        builder.Append(eventContext.ErrorUri);
-        builder.Append('\"');
-      }
-
-      Response.Headers.Append(HeaderNames.WWWAuthenticate, builder.ToString());
-    }
-  }
-
-  /// <inheritdoc />
-  protected override Task HandleForbiddenAsync(AuthenticationProperties properties)
-  {
-    var forbiddenContext = new ForbiddenContext(Context, Scheme, Options);
-    Response.StatusCode = 403;
-    return Events.Forbidden(forbiddenContext);
-  }
-
-  private static string CreateErrorDescription(Exception authFailure)
-  {
-    IReadOnlyCollection<Exception> exceptions;
-    if (authFailure is AggregateException agEx)
-    {
-      exceptions = agEx.InnerExceptions;
-    }
-    else
-    {
-      exceptions = new[] { authFailure };
-    }
-
-    var messages = new List<string>(exceptions.Count);
-
-    foreach (var ex in exceptions)
-    {
-      // Order sensitive, some of these exceptions derive from others
-      // and we want to display the most specific message possible.
-      switch (ex)
-      {
-        case SecurityTokenInvalidAudienceException stia:
-          messages.Add($"The audience '{stia.InvalidAudience ?? "(null)"}' is invalid");
-          break;
-        case SecurityTokenInvalidIssuerException stii:
-          messages.Add($"The issuer '{stii.InvalidIssuer ?? "(null)"}' is invalid");
-          break;
-        case SecurityTokenNoExpirationException _:
-          messages.Add("The token has no expiration");
-          break;
-        case SecurityTokenInvalidLifetimeException stil:
-          messages.Add("The token lifetime is invalid; NotBefore: "
-              + $"'{stil.NotBefore?.ToString(CultureInfo.InvariantCulture) ?? "(null)"}'"
-              + $", Expires: '{stil.Expires?.ToString(CultureInfo.InvariantCulture) ?? "(null)"}'");
-          break;
-        case SecurityTokenNotYetValidException stnyv:
-          messages.Add($"The token is not valid before '{stnyv.NotBefore.ToString(CultureInfo.InvariantCulture)}'");
-          break;
-        case SecurityTokenExpiredException ste:
-          messages.Add($"The token expired at '{ste.Expires.ToString(CultureInfo.InvariantCulture)}'");
-          break;
-        case SecurityTokenSignatureKeyNotFoundException _:
-          messages.Add("The signature key was not found");
-          break;
-        case SecurityTokenInvalidSignatureException _:
-          messages.Add("The signature is invalid");
-          break;
-      }
-    }
-
-    return string.Join("; ", messages);
   }
 }
